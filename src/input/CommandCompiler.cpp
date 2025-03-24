d88f696ca5cad6598a48f1367c3d6594545156a8
MicahMartin <micah.m329@gmail.com>
2025-03-24 16:56:46 -0400

    clean this shit up

--- src/input/CommandCompiler.cpp
+++ src/input/CommandCompiler.cpp
@@ -1,50 +1,9 @@
 #include <cstdio>
 #include <fstream>
 #include "CommandCompiler.h"
-#include "glaze/core/common.hpp"
-#include "glaze/core/context.hpp"
 #include "glaze/json/read.hpp"
 #include <glaze/glaze.hpp>
 
-// each 'commandString' is a descriptor for a stack of boolean function calls
-// P | ~P = ((wasPressed(LP)) || (wasReleased(LP)))
-// @F & !D = ((wasPressed(F, strict = false)) && !(wasPressed(D)))
-// MP & *D = ((wasPressed(MP)) && (isPressed(F)))
-// DF = (wasPressed(DF))
-// ~D = (wasReleased(D))
-
-//  Forward, neutral, forward
-//  "F, N, F",
-//  back , neutral, back 
-//  "B, N, B",
-//  any down release, neutral, any down press, LP
-//  "@~D, N, @D, LP",
-//  any forward that doesnt include down, neutral, forward (lienent dash)
-//  "@F & !D, N, F"
-//  any back that doesnt include down, neutral, forward (lienent backdash)
-//  "@B & !D, N, B",
-//  release of down, downforward, anyforward that doesnt include down, lk or release of lk (236K)
-//  "~D, DF, @F & !D, LK | ~LK",
-//  release of down, downback, any back that doesnt include down, lp or release of lp (214P)
-//  "~D, DB, @B & !D, LP | ~LP",
-//  MP + forward IS pressed
-//  "MP & *F",
-//  MP + back IS pressed 
-//  "MP & *B",
-//
-// input = N, F, B, U, D, UF, UB, DF, DB, LP, LK, MP, MK
-// funcMods = ~, *, @
-// unary = !
-// binary = &, |
-// TODO: unary and binary
-//  "MP & *F",
-//  "MP & *B",
-//  "@F & !D, N, F"
-//  "@B & !D, N, B",
-//  "~D, DF, @F & !D, LK | ~LK",
-//  "~D, DB, @B & !D, LP | ~LP",
-//  TODO: load from file
-//   "~D, 20DF, 20F, 8LP | 8~LP", // 214P
 CommandCompiler::CommandCompiler(){}
 
 CommandCompiler::~CommandCompiler(){}
@@ -60,8 +19,9 @@ void CommandCompiler::init(const char* path,
   std::string jsonBuff((std::istreambuf_iterator<char>(configFile)), std::istreambuf_iterator<char>());
   auto json = glz::read_json<RootJson>(jsonBuff);
   
-  for(auto commandObj : json->commands)
+  for(auto commandObj : json->commands){
     compile(commandObj.command.c_str(), commandObj.clears);
+  }
 
   printf("done compiling commands\n");
 }
@@ -73,13 +33,143 @@ void CommandCompiler::compile(const char* inputString, bool clears) {
   std::vector<CommandToken> tokens = commandScanner.scan(inputString);
   currentToken = &tokens[0];
 
-  while(currentToken->type != CTOKEN_END)
+  while(currentToken->type != CTOKEN_END){
     commandObj.command.push_back(compileNode());
+  }
 
   commands.push_back(commandObj);
 }
 
-CommandNode CommandCompiler::compileNode(){ return CommandNode();}
+CommandNode CommandCompiler::compileNode(){
+  // function pointer is &VirtualController::wasPressed by default
+  // bool strictness is true by default
+  using namespace std::placeholders;
+  // std::function<bool(Input, bool, int, bool)> funcPointer = std::bind(&VirtualController::wasPressedWrapper, controllerPointer, _1, _2, _3, _4);
+  CommandNode finalNode;
+  CommandFunction& finalFunc = finalNode.function;
+  bool strictness = true;
+  finalNode.bufferLength = 8;
+
+  while(currentToken->type != CTOKEN_DELIM && currentToken->type != CTOKEN_END) {
+    switch (currentToken->type) {
+      case CTOKEN_RELEASED: {
+        funcPointer = std::bind(&VirtualController::wasReleasedWrapper, controllerPointer, _1, _2, _3, _4);
+        currentToken++;
+      }
+      break;
+      case CTOKEN_HELD: {
+        funcPointer = std::bind(&VirtualController::isPressedWrapper, controllerPointer, _1, _2, _3, _4);
+        currentToken++;
+      }
+      break;
+      case CTOKEN_ANY: {
+        strictness = false;
+        currentToken++;
+      }
+      break;
+      case CTOKEN_NEUTRAL: {
+       finalFunc = std::bind(funcPointer, NOINPUT, strictness, _1, _2);
+       currentToken++;
+      }
+      break;
+      case CTOKEN_FORWARD: {
+       finalFunc = std::bind(funcPointer, RIGHT, strictness, _1, _2);
+       currentToken++;
+      }
+      break;
+      case CTOKEN_BACK: {
+        finalFunc = std::bind(funcPointer, LEFT, strictness, _1, _2);
+        currentToken++;
+      }
+      break;
+      case CTOKEN_UP: {
+        finalFunc = std::bind(funcPointer, UP, strictness, _1, _2);
+        printf("building up\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_DOWN: {
+        finalFunc = std::bind(funcPointer, DOWN, strictness, _1, _2);
+        printf("building down\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_UPFORWARD: {
+        finalFunc = std::bind(funcPointer, UPRIGHT, strictness, _1, _2);
+        printf("building upforward\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_UPBACK: {
+        finalFunc = std::bind(funcPointer, UPLEFT, strictness, _1, _2);
+        printf("building upback\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_DOWNFORWARD: {
+        finalFunc = std::bind(funcPointer, DOWNRIGHT, strictness, _1, _2);
+        printf("building upforward\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_DOWNBACK: {
+        finalFunc = std::bind(funcPointer, DOWNLEFT, strictness, _1, _2);
+        printf("building downback\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_LP: {
+        finalFunc = std::bind(funcPointer, LP, strictness, _1, _2);
+        printf("building lightpunch\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_LK: {
+        finalFunc = std::bind(funcPointer, LK, strictness, _1, _2);
+        printf("building lightk\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_MP: {
+        finalFunc = std::bind(funcPointer, MP, strictness, _1, _2);
+        printf("building mediumP\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_MK: {
+        finalFunc = std::bind(funcPointer, MK, strictness, _1, _2);
+        printf("building mediumKick\n");
+        currentToken++;
+      }
+      break;
+      case CTOKEN_NUMBER: {
+        finalNode.bufferLength = strtol(currentToken->start, NULL, 10);
+        currentToken++;
+        printf("building or\n");
+        finalFunc = binaryCommand(finalFunc, CTOKEN_OR);
+        printf("or control returned\n");
+      }
+      break;
+      case CTOKEN_AND: {
+        currentToken++;
+        printf("building and\n");
+        finalFunc = binaryCommand(finalFunc, CTOKEN_AND);
+      }
+      break;
+      default:
+        break;
+    }
+  }
+  if(currentToken->type != CTOKEN_END){
+    currentToken++;
+  }
+
+  // CommandTokenType type;
+  // const char* start;
+  // uint8_t length;
+
+  return finalNode;
+}
 
 
 CommandNode CommandCompiler::compileOneNode(){ return CommandNode(); }
